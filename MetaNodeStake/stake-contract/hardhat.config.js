b14f221869d29a983589b9ccacea2670c9b0630b
liyanyan <liyanyan@showmac.com>
2026-03-16 19:02:55 +0800

    metanode test

--- MetaNodeStake/stake-contract/hardhat.config.js
+++ MetaNodeStake/stake-contract/hardhat.config.js
@@ -13,14 +13,14 @@ module.exports = {
       },
     },
   },
-  networks: {
-    sepolia: {
-      url: "https://sepolia.infura.io/v3/d8ed0bd1de8242d998a1405b6932ab33",
-      // url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 30000000000, // 30 Gwei
-    },
-  },
+  // networks: {
+  //   sepolia: {
+  //     url: "https://sepolia.infura.io/v3/d8ed0bd1de8242d998a1405b6932ab33",
+  //     // url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+  //     accounts: [process.env.PRIVATE_KEY],
+  //     gasPrice: 30000000000, // 30 Gwei
+  //   },
+  // },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || '5FCIE4WK4IU1DNCAGGBXNZJPK49YMIS5FT',
   },
